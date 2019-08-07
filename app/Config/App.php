997edb168da55c7ec85ab139d6667490c0380651
Jerome Dh <jdieuhou@yahoo.fr>
2019-08-07 17:26:29 +0100

    Configuration file

---
+++ app/Config/App.php
@@ -0,0 +1,28 @@
+<?php
+ 
+namespace App\Config;
+
+
+//==============================================================================
+//
+//	Configuration de l'application
+// 
+// 	Les multiples informations sur l'application
+//	NB: Chaque nom de constante doit être en majuscule
+//
+//
+//	@date 06/08/2019
+//
+//	@author Jerome Dh <jdieuhou@gmail.com>
+//
+//=============================================================================
+
+
+class App 
+{
+	const NAME = 'Dhyno';
+
+	const VERSION = '1.0';
+
+}
+	
