6150791960941dd7558f2453372b08e0721d413f
Jerome Dh <jdieuhou@yahoo.fr>
2019-08-07 17:30:21 +0100

    The layout file

---
+++ app/Views/Templates/layout.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+	<head dir="ltr">
+
+		<meta charset="utf-8">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<link rel="icon" href="images/logo.png" type="image/png" sizes="16x16">
+
+		<title><?= app()->name() ?></title>
+
+		<style type="text/css">		
+
+		</style>
+
+	</head>
+	
+	<body>
+
+		<nav>
+			<div class="container">
+
+			</div>
+		</nav>
+
+		<main>
+
+			<div class="container">
+
+				<h1>Dhyno Framework</h1>
+
+				<p>Une expérience construite sur le Web</p>
+
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+
+			</div>
+
+		</main>
+
+		<footer>
+			<div class="container">
+			</div>
+		</footer>
+	
+  </body>
+
+</html>
