09685338724ed5b4f5cc6cd30045a0ffb3d7156a
Jerome Dh <jdieuhou@yahoo.fr>
2019-08-07 17:43:25 +0100

    Cmd for testing

---
+++ phpunit.cmd
@@ -0,0 +1,18 @@
+
+REM Lancer PHPUnit
+REM @By Jerome Dh <jdieuhou@gmail.com>
+
+@echo off
+
+cls
+
+title PHPUnit By Jerome Dh
+
+:debut
+
+php vendor\phpunit\phpunit\phpunit %*
+
+:fin 
+
+
+@echo on
