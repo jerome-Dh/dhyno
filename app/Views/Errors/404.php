e18eca8488cb94c833dc274ecd9357adc43620fc
Jerome Dh <jdieuhou@yahoo.fr>
2019-08-07 17:28:39 +0100

    Creating error 404 page

---
+++ app/Views/Errors/404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+
+	<head>
+	
+		<title>
+			Page introuvable
+		</title>
+	
+		<meta http-equiv="Content-type" content="text/html; charset=iso-8859-1" />
+		<link rel="stylesheet" href="/css/Envision.css" type="text/css"/>
+	</head>
+	
+	<body>
+
+		<div id="wrap">
+
+			<h2><pre><?php echo htmlentities($msg); ?></pre></h2>
+
+			<div id="footer"></div>
+
+		</div>
+
+	</body>
+
+</html>
